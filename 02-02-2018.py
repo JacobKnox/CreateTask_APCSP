112025bffc0216feede3dc03be8d691bedabdd95
Jacob Knox <jacobknox@dixie.k12.fl.us>
2018-02-02 09:40:59 -0500

    Update 02-02-2018.py

--- 02-02-2018.py
+++ 02-02-2018.py
@@ -1,40 +1,84 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Bullying: A Pandemic Problem</title>
-	</head>
-	<body>
-		<h1 style=color:blue; align=center>What is bullying?</h1>
-		<p style=color:blue; align=left>Bullying, as defined by <a style=color:red; href="https://www.stopbullying.gov/what-is-bullying/index.html"><b>stopbullying.gov</b></a>, is <em>"unwanted, aggressive behavior among school aged children that involves a real or perceived power imbalance. The behavior is repeated, or has the potential to be repeated, over time. Both kids who are bullied and who bully others may have serious, lasting problems."</em></p>
-		<h1 style=color:blue; align=center>Types of Bullying</h1>
-		<img align=right style=width:300px; src="http://facts.net/wp-content/uploads/2015/05/Where-does-bullying-happen.jpg">
-		<ul style=color:blue; align=left>
-		  <br>
-		  <li>Verbal bullying - saying or writing mean things: Teasing, name-calling, inappropriate sexual comments, taunting, and threatening to cause harm.</li>
-		  <br>
-		  <li>Social bullying - hurting someone’s reputation or relationships: Leaving someone out on purpose, telling other children not to be friends with someone, spreading rumors about someone, and embarrassing someone in public.</li>
-		  <br>
-		  <li>Physical bullying - hurting a person’s body or possessions: Hitting/kicking/pinching, spitting, tripping/pushing, taking or breaking someone’s things, and making mean or rude hand gestures.</li>
-		  <br><br>
-		</ul>
-		<h1 style=color:blue; align=center>How to Stop Bullying</h1>
-		<ol style=color:blue;>
-		  <p><b><a style=color:red; href="https://www.stopbullying.gov/what-you-can-do/index.html">Parents</a></b></p>
-		  <li>"Recognize the warning signs that your child is involved in bullying."</li>
-		  <li>"Learn what bullying is and what it is not."</li>
-		  <li>"Learn how to work with your kids to prevent cyberbullying and how to respond when it occurs."</li>
-		  <li>"Utilize tips and tools to talk to your child about bullying."</li>
-		  <li>"If you know or suspect bullying has occurred, learn how to find out what has happened with your child."</li>
-		  <li>"If you have determined bullying has occurred, learn how you and school or community officials can work together to support your child, whether they were bullied, bullied others, or witnessed bullying.  Learn also about considerations for specific groups."</li>
-		  <li>"If bullying is occurring at school, learn about what your state requires schools to do in your state’s anti-bullying law. Learn also about federal laws that require schools to address harassment based on race, color, national origin, sex, and disabilities and ways to report situations that have not been adequately addressed to the U.S. Departments of Education and Justice."</li>
-		  <li>"If you have worked with your child and your school and need additional assistance, find resources to help address the situation."</li>
-		  <p><b><a style=color:red; href="https://www.stopbullying.gov/what-you-can-do/teens/index.html">Teens</a></b></p>
-		  <li>"Learn more about cyberbullying and how to respond if it is happening to you."</li>
-		  <li>"Learn how you can be more than a bystander."</li>
-		  <li>"SPEAK UP: If you feel uncomfortable with the comments or actions of someone… tell someone! It is better to let a trusted adult know, than to let the problem continue."</li>
-		</ol>
-		<footer>
-		  <p style=color:orange; align=center><em>Written By: <a style=color:orange; href="mailto:jacobknox@dixie.k12.fl.us">Jacob Knox</a></em></p>
-		</footer>
-	</body>
-</html>
+print("Hello! Welcome to your personal profile creator!")
+print("________________________________________________")
+name = input("What's your name?")
+print("Nice to meet you " + name + "!")
+if input("May I know more about you in order to form your profile?") == "Yes":
+  hometown = input("Where were you born?")
+  currenttown = input("Where do you currently live?")
+  age = input("How old are you?")
+  def WhatCanYouDo():
+    if int(age) < 18:
+      print("You are a minor!")
+    if int(age) >= 16:
+	    print("You can drive!")
+    if int(age) >= 18:
+      print("You can vote!")
+    if int(age) >= 21:
+	    print("You can drink!")
+    if int(age) >= 25:
+	    print("You can rent a car!")
+  DOBM = input("What month were you born in?")
+  DOBD = input("What day were you born on?")
+  def ZodiacSign():
+    if DOBM == "January":
+      if int(DOBD) >= 20:
+        print("You're an Aquarius!")
+      else:
+        print("You're a Capricorn!")
+    elif DOBM == "February":
+      if int(DOBD) <= 18:
+        print("You're an Aquarius!")
+      else:
+        print("You're a Pisces!")
+    elif DOBM == "March":
+      if int(DOBD) <= 20:
+        print("You're a Pisces!")
+      else:
+        print("You're an Aries!")
+    elif DOBM == "April":
+      if int(DOBD) <= 19:
+        print("You're an Aries!")
+      else:
+        print("You're a Taurus!")
+    elif DOBM == "May":
+      if int(DOBD) <= 20:
+        print("You're a Taurus!")
+      else:
+        print("You're a Gemini!")
+    elif DOBM == "June":
+      if int(DOBD) <= 20:
+        print("You're a Gemini!")
+      else:
+        print("You're a Cancer!")
+  year = 2017
+  DOBY = str(year - int(age))
+  def WhenWereYouBorn():
+    print("You were born on: " + DOBM + " " + DOBD + ", " + DOBY)
+  def Profile():
+    print("________")
+    print("PROFILE:")
+    print("________")
+    print("Name: " + name)
+    WhenWereYouBorn()
+    WhatCanYouDo()
+    ZodiacSign()
+    print("Born in: " + hometown)
+    print("Lives in: " + currenttown)
+  if input("Would you like to know your profile?") == "Yes":
+    Profile()
+    if input("Would you like to lock your profile?") == "Yes":
+      password = input("What would you like to set your password to?")
+      print("You're profile has been locked! You can reaccess your profile by entering: 'I'd like to see my profile'!")
+    else:
+      print("Alright! Let me know if you need anything! You can reaccess your profile by entering: 'I'd like to see my profile'!")
+  else:
+    print("Alright! Let me know if you need anything!")
+else:
+  print("Alright! Let me know if you need anything!")
+if input("") == "I'd like to see my profile":
+  if input("What is your password?") == password:
+    Profile()
+else:
+  print("How can I help you?")
+if input("") == "What is your name?":
+  print("My name is PPC Bot--your personal profile creator bot! But you may simply call me Charles.")
